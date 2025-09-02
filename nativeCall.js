9bcc88402a0584b9432ba00062d0dca4cb5fd05a
Abdul Kioum <101413887+Abdul-Kioum@users.noreply.github.com>
2025-09-02 19:04:23 +0600

    Add files via upload

---
+++ nativeCall.js
@@ -0,0 +1,23 @@
+function nativeCall(fn, args = [], types = [], ret = "void") {
+    // If types not given, auto-detect
+    if (!types.length) {
+      types = args.map(v => {
+        if (typeof v === "number") return Number.isInteger(v) ? "int" : "double";
+        if (typeof v === "boolean") return "boolean";
+        return "string";
+      });
+    }
+
+    // Always stringify args
+    const strArgs = args.map(v => String(v));
+
+    // Payload JSON
+    const payload = JSON.stringify({
+      fn, ret,
+      args: strArgs,
+      argTypes: types
+    });
+
+    // Send to Android
+    return prompt("CLIENT", payload) || "";
+  }
